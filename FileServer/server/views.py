135b8db0be2e90218be08ceaf79c77dc15ff29e9
Gideon Tettehfio <gideonlambride@gmail.com>
2024-07-06 14:48:24 +0000

    custom user model to use email as username

--- FileServer/server/views.py
+++ FileServer/server/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import SignUpForm
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('<h1>page setup</h1>')
+    return HttpResponse('<h1>Home page</h1>')
+
+
+def signup(request):
+    form = SignUpForm()
+
+    context = {'form': form}
+    return render(request, 'signup.html', context)
