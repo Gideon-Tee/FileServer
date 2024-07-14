b05d76b84503b6193bdfffc9f1fe5a34f92b4fdf
Gideon Tettehfio <gideonlambride@gmail.com>
2024-07-14 20:50:36 +0000

    added custom 404 page

--- FileServer/FileServer/urls.py
+++ FileServer/FileServer/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('server.urls')),
     path('accounts/', include('allauth.urls')),
 ]
+
+handler404 = 'django.views.defaults.page_not_found'
